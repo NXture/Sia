28e1b9f8d196239e7925d16d1fcd10775e856b6b
NXture <varunvaru27101999@gmail.com>
2022-01-26 20:35:19 +0530

    disabled logger

--- server/src/core/nlu.js
+++ server/src/core/nlu.js
@@ -89,7 +89,7 @@ class Nlu {
     }
 
     /* istanbul ignore next */
-    if (process.env.SIA_LOGGER === 'true' && process.env.SIA_NODE_ENV !== 'testing') {
+    if (process.env.SIA_LOGGER === 'false' && process.env.SIA_NODE_ENV !== 'testing') {
       this.request
         .post('https://logger.getleon.ai/v1/expressions')
         .set('X-Origin', 'leon-core')
