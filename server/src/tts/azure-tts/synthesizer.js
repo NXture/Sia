f14eb971971ad80679d20fb9cfb3db74bef58601
nxture <varunvaru27101999@gmail.com>
2022-03-26 03:14:59 +0530

    chore: fixed typos

--- server/src/tts/azure-tts/synthesizer.js
+++ server/src/tts/azure-tts/synthesizer.js
@@ -1,23 +1,23 @@
-import * as sdk from "microsoft-cognitiveservices-speech-sdk";
-import { SpeechSynthesizer } from "microsoft-cognitiveservices-speech-sdk";
-import Ffmpeg from "fluent-ffmpeg";
-import { path as ffmpegPath } from "@ffmpeg-installer/ffmpeg";
-import { path as ffprobePath } from "@ffprobe-installer/ffprobe";
-import fs from "fs";
+import * as sdk from 'microsoft-cognitiveservices-speech-sdk'
+import { SpeechSynthesizer } from 'microsoft-cognitiveservices-speech-sdk'
+import Ffmpeg from 'fluent-ffmpeg'
+import { path as ffmpegPath } from '@ffmpeg-installer/ffmpeg'
+import { path as ffprobePath } from '@ffprobe-installer/ffprobe'
+import fs from 'fs'
 
-import log from "../../helpers/log.js";
-import string from "../../helpers/string.js";
+import log from '../../helpers/log.js'
+import string from '../../helpers/string.js'
 
-log.title("Azure TTS Synthesizer");
+log.title('Azure TTS Synthesizer')
 
-const synthesizer = {};
+const synthesizer = {}
 const voices = {
-  "en-US": {
-    voice: "en-US-AmberNeural",
+  'en-US': {
+    voice: 'en-US-AmberNeural',
   },
-};
-let client = {};
-const file = `${__dirname}/../../tmp/${Date.now()}-${string.random(4)}.wav`;
+}
+let client = {}
+const file = `${__dirname}/../../tmp/${Date.now()}-${string.random(4)}.wav`
 
 /**
  * Initialize Azure Text-to-Speech based on credentials in the JSON file
@@ -25,22 +25,22 @@ const file = `${__dirname}/../../tmp/${Date.now()}-${string.random(4)}.wav`;
 
 synthesizer.init = () => {
   const config = JSON.parse(
-    fs.readFileSync(`${__dirname}/../../config/voice/azure-tts.json`, "utf8")
-  );
+    fs.readFileSync(`${__dirname}/../../config/voice/azure-tts.json`, 'utf8')
+  )
   const speechConfig = sdk.SpeechConfig.fromSubscription(
     config.key,
     config.region
-  );
-  const audioConfig = sdk.AudioConfig.fromAudioFileOutput(file);
-  speechConfig.speechSynthesisLanguage = "en-US";
-  speechConfig.speechSynthesisVoiceName = voices[process.env.SIA_LANG].voice;
+  )
+  const audioConfig = sdk.AudioConfig.fromAudioFileOutput(file)
+  speechConfig.speechSynthesisLanguage = 'en-US'
+  speechConfig.speechSynthesisVoiceName = voices[process.env.SIA_LANG].voice
   try {
-    client = new SpeechSynthesizer(speechConfig, audioConfig);
-    log.success("Synthesizer initialized");
+    client = new SpeechSynthesizer(speechConfig, audioConfig)
+    log.success('Synthesizer initialized')
   } catch (e) {
-    log.error(`Azure TTS: ${e}`);
+    log.error(`Azure TTS: ${e}`)
   }
-};
+}
 
 /**
  * Save string to audio file
@@ -48,30 +48,30 @@ synthesizer.init = () => {
 
 synthesizer.save = (speech, em, cb) => {
   client.speakTextAsync(speech, ({ result }) => {
-    const wStream = fs.createWriteStream(file);
+    const wStream = fs.createWriteStream(file)
 
-    result.pipe(wStream);
+    result.pipe(wStream)
 
-    wStream.on("finish", () => {
-      const ffmpeg = new Ffmpeg();
-      ffmpeg.setFfmpegPath(ffmpegPath);
-      ffmpeg.setFfprobePath(ffprobePath);
+    wStream.on('finish', () => {
+      const ffmpeg = new Ffmpeg()
+      ffmpeg.setFfmpegPath(ffmpegPath)
+      ffmpeg.setFfprobePath(ffprobePath)
 
       // Get file duration thanks to ffprobe
       ffmpeg.input(file).ffprobe((err, data) => {
-        if (err) log.error(err);
+        if (err) log.error(err)
         else {
-          const duration = data.streams[0].duration * 1000;
-          em.emit("saved", duration);
-          cb(file, duration);
+          const duration = data.streams[0].duration * 1000
+          em.emit('saved', duration)
+          cb(file, duration)
         }
-      });
-    });
+      })
+    })
 
-    wStream.on("error", (err) => {
-      log.error(`Azure TTS: ${err}`);
-    });
-  });
-};
+    wStream.on('error', (err) => {
+      log.error(`Azure TTS: ${err}`)
+    })
+  })
+}
 
-export default synthesizer;
+export default synthesizer
